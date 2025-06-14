a73000adf61a7fb6e908c6807a8c05904ef253a4
Layron <dzarudnyi.work@gmail.com>
2025-06-14 16:49:11 +0300

    Created project structure, added packages

---
+++ DTTestTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DTTestTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
